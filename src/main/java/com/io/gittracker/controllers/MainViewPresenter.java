3383fcc257dcec468888fb6416ce6d0e87e0c27a
Szymon Głomski <szymon.glomski@gmail.com>
2024-03-20 17:25:00 +0100

    add basic styling

--- src/main/java/com/io/gittracker/controllers/MainViewPresenter.java
+++ src/main/java/com/io/gittracker/controllers/MainViewPresenter.java
@@ -1,16 +1,24 @@
 package com.io.gittracker.controllers;
 
 import com.io.gittracker.UIMain;
+import com.io.gittracker.model.AppState;
+import com.io.gittracker.model.Workspace;
 import com.io.gittracker.services.TokenService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.BorderPane;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Component
 public class MainViewPresenter {
     private final TokenService tokenService;
     private final UIMain uiMain;
+    private AppState appState = new AppState( new ArrayList<>(List.of(new Workspace("Inżynieria Oprogramowania",null), new Workspace("Technologie obiektowe",null))),0,0,0,new Date());
     @FXML
     private ListView<String> classes;
 
@@ -21,8 +29,11 @@ public class MainViewPresenter {
     private ListView<String> other;
 
     public void initialize() {
+        ObservableList<String> items = FXCollections.observableArrayList(
+                appState.getWorkspaces().stream().map(Workspace::getName).toList()
+        );
         // Add sample items to the lists
-        classes.getItems().addAll("Inżynieria Oprogramowania", "Technologie Obiektowe", "Projektowanie Obiektowe");
+        classes.setItems(items);
         groups.getItems().addAll("Grupa 1", "Grupa 2", "Grupa 4", "Grupa 4");
         other.getItems().addAll("Graded", "Not Graded", "Overdue", "Not Overdue");
     }
