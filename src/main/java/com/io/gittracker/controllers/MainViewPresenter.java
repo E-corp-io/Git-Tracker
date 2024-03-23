1ae66398400e0d470852c75a11963b7394cc0de1
wloboz <weronika.loboz@gmail.com>
2024-03-23 15:23:55 +0100

    add new repo
    added due date field
    added window for entering new repo details
    added clickable "add new repo" label
    added stylesheets to fxml files

--- src/main/java/com/io/gittracker/controllers/MainViewPresenter.java
+++ src/main/java/com/io/gittracker/controllers/MainViewPresenter.java
@@ -4,10 +4,20 @@ import com.io.gittracker.UIMain;
 import com.io.gittracker.model.AppState;
 import com.io.gittracker.model.Workspace;
 import com.io.gittracker.services.TokenService;
+import java.io.IOException;
+import java.util.Objects;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,6 +35,9 @@ public class MainViewPresenter {
     @FXML
     private ListView<String> other;
 
+    @FXML
+    private Label newRepoLabel;
+
     public void initialize() {
         ObservableList<String> items = FXCollections.observableArrayList(
                 appState.getWorkspaces().stream().map(Workspace::getName).toList());
@@ -41,5 +54,19 @@ public class MainViewPresenter {
     public MainViewPresenter(TokenService tokenService, UIMain uiMain) {
         this.tokenService = tokenService;
         this.uiMain = uiMain;
+    }
+
+    @FXML
+    public void handleAddNewRepoClicked(MouseEvent event) throws IOException {
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        Node node = (Node) event.getSource();
+        Stage thisStage = (Stage) node.getScene().getWindow();
+        popupStage.initOwner(thisStage);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/addRepo.fxml")));
+        Scene popupScene = new Scene(root);
+        popupStage.setScene(popupScene);
+        popupStage.setTitle("Add new repo");
+        popupStage.show();
     }
 }
